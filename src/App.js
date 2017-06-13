aea1260436ac47a2bc6fbd37c9d9198d4bd6557b
Michael OConnell <michaeldavidoconnell@gmail.com>
2017-06-13 10:42:11 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import React, { Component } from 'react';
 import UserInput from './components/UserInput'
-import Users from './components/Users'
-import {connect} from 'react-redux';
+import { ConnectedUsers } from './components/Users'
+import { connect } from 'react-redux';
 export class App extends Component {
   render() {
     return (
       <div className="App">
         <UserInput store={this.props.store}/>
-        <Users store={this.props.store}/>
+        <ConnectedUsers />
       </div>
     );
   }
 }
 
-const mapStateToProps = (state) => { 
-  return { users: state.users };
-};
- 
-export default connect(mapStateToProps)(App);
+export default App;
--- src/components/Users.js
+++ src/components/Users.js
@@ -3,20 +3,25 @@ import { connect } from 'react-redux';
 
 export class Users extends Component {
 
-  render() {
-
-    return (
-      <div>
-        <ul>
-          {/* stuff should happen around here */}
-        </ul>
-      </div>
-    )
-  }
+    render() {
+        const users = this.props.users.map( (user, index) => {
+            return (<li key={index}>{user.userName}</li>);
+        })
+        return (
+            <div>
+                <ul>
+                    {users}
+                </ul>
+            </div>
+        )
+    }
 }
 const mapStateToProps = (state) => { 
-  return { users: state.users };
+    return { 
+        users: state.users,
+        primaryUser: state.users[0],
+    };
 };
- 
+
 export const ConnectedUsers = connect(mapStateToProps)(Users) // aren't we supposed to be connecting something around here?
 
