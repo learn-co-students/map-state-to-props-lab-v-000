696f97f972b4ca118a8cd9aa7139f0849f9d75a2
Phreemason <github email address>
2017-06-23 21:35:18 -0400

    Done.

--- src/components/Users.js
+++ src/components/Users.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 export class Users extends Component {
 
   render() {
-    const users = this.props.users.map((user, index)=> <li key="{index}">{user.userName}</li>);
+    const users = this.props.users.map((user, index)=>(<li key={index}>{user.userName}</li>))
     return (
       <div>
         <ul>
