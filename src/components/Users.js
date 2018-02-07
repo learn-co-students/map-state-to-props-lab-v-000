4fe76d662c81a57483a3c02bd7f4bf94407cfbcf
Dembe Koi <stephanos@goldmail.etsu.edu>
2018-02-07 16:48:03 -0500

    Done.

--- src/components/Users.js
+++ src/components/Users.js
@@ -4,8 +4,7 @@ import { connect } from 'react-redux';
 class Users extends Component {
 
   render() {
-
-  	let users = this.props.users.map((user, index) => <li key={index}>{user.userName}</li>);
+    let users = this.props.users.map((user, index) => <li key={index}>{user.userName}</li>);
 
     return (
       <div>
@@ -20,9 +19,8 @@ class Users extends Component {
 const mapStateToProps = (state) => {
   return {
     users: state.users,
-    primaryUser: state.users[0]
+    primaryUser: state.users[0],
   };
 };
 
-export const ConnectedUsers = connect(mapStateToProps)(Users);
-
+export defualt const ConnectedUsers = connect(mapStateToProps)(Users);
