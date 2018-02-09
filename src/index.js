876334a821d7d90884b45edac9e5d59373b68475
ndalcin <dalcin.nicole@gmail.com>
2018-02-09 11:17:12 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import manageBand from './reducers/manageBand';
+import manageUser from './reducers/manageUser';
 
 // we wrap store in a function for testing purposes
 export const configureStore = () => {
-  return createStore(manageBand, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+  return createStore(manageUser, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 };
 
 let store = configureStore();
