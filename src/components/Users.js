d538324dec2cdc45b41f3dcc3d8a982902ed9375
emhopkins <epoletto@lstreetc.com>
2018-02-15 22:46:07 -0600

    Done.

--- src/components/Users.js
+++ src/components/Users.js
@@ -8,12 +8,15 @@ export class Users extends Component {
     return (
       <div>
         <ul>
-          {/* stuff should happen around here */}
+          {this.props.users.map((user, index) => { return <li key={index}>{user.userName} is from {user.hometown}</li>})}
         </ul>
       </div>
     )
   }
 }
 
-export const ConnectedUsers = Users // aren't we supposed to be connecting something around here?
-
+const mapStateToProps = (state) => { 
+  return { users: state.users, primaryUser: state.users[0]}
+};
+ 
+export const ConnectedUsers = connect(mapStateToProps)(Users)
