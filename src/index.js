8d403124a8bcbd768b3cb600e007c839226b9fd8
Sophia Varella <sophiavarella@gmail.com>
2018-06-08 21:12:53 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import manageBand from './reducers/manageBand';
 
+
 // we wrap store in a function for testing purposes
 export const configureStore = () => {
   return createStore(manageBand, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
