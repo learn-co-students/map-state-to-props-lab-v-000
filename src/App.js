672a855eb5eb0c152727e2204df26f17012b74d5
pcherian10 <pcherian10@gmail.com>
2019-01-16 18:19:24 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import UserInput from './components/UserInput'
 import ConnectedUsers from './components/Users'
 
+
 class App extends Component {
+
   render() {
     return (
       <div className="App">
--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
 
 class UserInput extends Component {
 
