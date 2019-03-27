7ff7e1faa3b2380163705f2102ecb2a07a05054e
terryblue99 <terryblue@comcast.net>
2019-03-27 17:44:53 -0500

    Done.

--- src/components/Users.js
+++ src/components/Users.js
@@ -10,7 +10,7 @@ class Users extends Component {
     return (
       <div>
       	<br />
-      	<label>Users </label> {this.props.userCount}
+      	<label>Users: </label> {this.props.userCount}
         <ul>
           {users}
         </ul>
