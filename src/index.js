1f7b3c831151b8ba66248ee7519c65f36e6037d1
Bruce Hillsworth <brucehillsworth@Bruces-MacBook-Air.local>
2019-04-09 21:12:17 -0600

    Passed Tests

--- src/index.js
+++ src/index.js
@@ -3,15 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './App'
 
 import manageUsers from './reducers/manageUsers'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 
 
-// add imports and code
+const store = createStore(manageUsers, window._REDUX_DEVTOOLS_EXTENSION_ && window._REDUX_DEVTOOLS_EXTENSION_())
 
 
 ReactDOM.render(
-  // add imports and code
-  <App />
-  // add imports and code
-  ,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
