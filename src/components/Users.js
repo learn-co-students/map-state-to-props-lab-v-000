ad2fa9085784c80c575a21675359a00b93110f6d
github username <github email address>
2019-04-24 16:48:58 -0400

    Done.

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
-
+import { connect } from 'react-redux'
 class Users extends Component {
 
   render() {
+    const userlist = (this.props.users)
+    debugger
     return (
       <div>
         <ul>
-          Users!
+          Users! {this.props.users.length}
         </ul>
+        Userlist: {
+          this.props.users.map(user => (
+            <ul>
+              <li>{user.username}</li>
+            </ul>
+          ))
+        }
       </div>
     )
   }
 }
 
-//add mapStateToProps here
 
-export default Users
+
+//add mapStateToProps here
+const mapStateToProps = (state) => {
+  return { users: state.users };
+};
+export default connect(mapStateToProps)(Users);
