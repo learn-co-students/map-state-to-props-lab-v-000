3772c6c966df953d3f89da0361a8e30eaf69ce05
roitblatari <roitblatari@gmail.com>
2019-06-19 15:02:29 -0400

    Done.

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
+import { connect } from 'http2';
 
 class Users extends Component {
-
-  render() {
-    return (
-      <div>
-        <ul>
-          Users!
-        </ul>
-      </div>
-    )
-  }
+	render() {
+		debugger;
+		return (
+			<div>
+				<ul>Users! {this.props.users}</ul>
+			</div>
+		);
+	}
 }
 
 //add mapStateToProps here
-
-export default Users
+function mapStateToProps(state) {
+	debugger;
+	return { users: state.users };
+}
+export default connect()(Users);
