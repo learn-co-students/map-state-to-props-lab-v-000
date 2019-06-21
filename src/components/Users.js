8f5fab1ea2f9c19aa16294325c1df48025183221
github username <github email address>
2019-06-21 08:36:59 -0500

    Done.

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
-
+import { connect } from 'react-redux'
 class Users extends Component {
 
   render() {
     return (
       <div>
         <ul>
-          Users!
+          Users! {this.props.users.length}
+          {this.props.users.map(user => <li key={user.username}>Name: {user.username}, Hometown: {user.hometown}</li>)}
         </ul>
       </div>
     )
   }
 }
 
+const mapStateToProps = (state) => {
+  return { users: state.users }
+}
+
 //add mapStateToProps here
 
-export default Users
+export default connect(mapStateToProps)(Users)
--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App'
-
-import manageUsers from './reducers/manageUsers'
+import App from './App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import manageUsers from './reducers/manageUsers';
 
 
 // add imports and code
-
+const store = createStore(
+  manageUsers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   // add imports and code
-  <App />
+  <Provider store={store}>
+    <App />
+  </Provider>
   // add imports and code
   ,
   document.getElementById('root')
