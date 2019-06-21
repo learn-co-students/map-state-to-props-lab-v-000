ce476dde21810853cc89ff038a272303f8286825
roitblatari <roitblatari@gmail.com>
2019-06-21 10:16:44 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { Provider } from 'react-redux';
 import manageUsers from './reducers/manageUsers';
 
 // add imports and code
-const store = createStore(manageUsers);
+const store = createStore(manageUsers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
 	// add imports and code
