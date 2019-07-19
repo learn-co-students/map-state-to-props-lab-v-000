412f73585e1b3ff2b6901fb842dc23f1689a260c
Juliana Fogg <julianafogg@Julianas-MBP.fios-router.home>
2019-07-19 13:50:58 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import UserInput from './components/UserInput'
 import ConnectedUsers from './components/Users'
 
--- src/components/Users.js
+++ src/components/Users.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
 
 class Users extends Component {
+
+
+renderUsers = () => (
+  this.props.users.map(u => (
+    <li>
+      <h2>{u.username}</h2>
+      <h3>{u.hometown}</h3>
+    </li>
+  ))
+)
 
   render() {
     return (
       <div>
+          {`${this.props.count} users:`}
         <ul>
-          Users!
+          {this.renderUsers()}
         </ul>
       </div>
     )
@@ -15,4 +28,6 @@ class Users extends Component {
 
 //add mapStateToProps here
 
-export default Users
+const mapStateToProps = (({ users }) => ({ users, count: users.length }))
+
+export default connect(mapStateToProps)(Users);
