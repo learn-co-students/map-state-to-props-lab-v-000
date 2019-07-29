ac604a75ca7c456e13699952a8eb8ea69d8a3115
Jamie Giuliano <jamiepgiuliano@gmail.com>
2019-07-29 15:43:05 -0700

    Done.

--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -17,11 +17,12 @@ class UserInput extends Component {
   handleOnSubmit = (event) => {
     event.preventDefault();
     this.props.dispatch({type: 'ADD_USER', user: this.state})
+    document.getElementById("user-form").reset();
   }
 
   render() {
     return(
-      <form onSubmit={this.handleOnSubmit}>
+      <form id="user-form" onSubmit={this.handleOnSubmit}>
         <p>
           <input
             type="text"
