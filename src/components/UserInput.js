5635fd1bb046ec3196244e71515a30bbece31a9e
Pamela Frieze Torres-Rocca <pamela@Morpheus.local>
2019-11-27 23:00:19 -0500

    Done.

--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
+import '../App.js';
+
 
 class UserInput extends Component {
 
--- src/components/Users.js
+++ src/components/Users.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import '../App.js';
+
+
 // add any needed imports here
 class Users extends Component {
 
@@ -7,7 +11,7 @@ class Users extends Component {
       <div>
         <ul>
           Users!
-          {/* Write code here that displays the usernames of all users in the Redux store */}
+          {}
           {/* In addition, display the total number of users curently in the store */}
         </ul>
       </div>
@@ -16,6 +20,9 @@ class Users extends Component {
 }
 
 //add mapStateToProps here
+const mapStateToProps = (state) => {
+  return { users: state.users };
+};
 
 // connect this component to Redux
-export default Users
+export default connect()(Users);
