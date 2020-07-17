64946e6a2843c04f3e191b5f38ad83c229e7ec9e
iamtamtamiam <vutam_a@hotmail.com>
2020-07-17 13:27:06 -0500

    Done.

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 // add any needed imports here
+//Connect the Users component similar to how it is connected in UserInput.
+import { connect } from 'react-redux';
 class Users extends Component {
 
   render() {
+  let displayUsers = this.props.users.map(user => <li>{user.username}</li>)
     return (
       <div>
         <ul>
           Users!
           {/* Write code here that displays the usernames of all users in the Redux store */}
+          {displayUsers}
           {/* In addition, display the total number of users curently in the store */}
+          {this.props.userCount}
         </ul>
       </div>
     )
@@ -16,6 +21,13 @@ class Users extends Component {
 }
 
 //add mapStateToProps here
+const mapStateToProps = state => {
+  return {
+    users: state.users,
+    userCount: state.users.length
+  };
+};
 
 // connect this component to Redux
-export default Users
+// export default Users
+export default connect(mapStateToProps)(Users)
--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'
-
+//use the createStore from redux, 
+import { createStore } from 'redux';
+//Use Provider from react-redux to wrap <App />, 
+import { Provider } from 'react-redux';
 import manageUsers from './reducers/manageUsers'
 
 
-// add imports and code
+// passing in the provided reducer, manageUsers, to create a store
 
+const store = createStore(
+  manageUsers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
+//passing store as a prop to the Provider.
 ReactDOM.render(
-  // add imports and code
-  <App />
-  // add imports and code
+ 
+  <Provider store={store}>
+    <App />
+  </Provider>
+
   ,
   document.getElementById('root')
 );
