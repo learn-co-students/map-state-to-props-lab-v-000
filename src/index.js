67697f07944d95ec59597cd788aa1a625a041982
sdcro <sdcrouse91@gmail.com>
2020-08-17 18:23:29 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import manageUsers from './reducers/manageUsers'
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
-const store = createStore(manageUsers);
+const store = createStore(manageUsers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
   <Provider store={store}>
