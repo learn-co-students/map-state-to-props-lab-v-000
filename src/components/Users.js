bff5ef269bbc2103b2114258bd6d3ddbb3d2f203
Leslie Stevens <flymaven@protonmail.com>
2020-11-02 19:22:23 -0600

    add key for userCount

--- src/components/Users.js
+++ src/components/Users.js
@@ -8,14 +8,14 @@ class Users extends Component {
     const userNames = this.props.users.map(user => (
       <li>{user.username}</li>
     ))
-    const userNumber = this.props.users.length
+    //const userNumber = this.props.users.length
     return (
       <div>
         <ul>
           Users!
           {userNames
           /* Write code here that displays the usernames of all users in the Redux store */}
-          {userNumber/* In addition, display the total number of users curently in the store */}
+          {this.props.userCount/* In addition, display the total number of users curently in the store */}
         </ul>
       </div>
     )
@@ -23,11 +23,11 @@ class Users extends Component {
 }
 
 const mapStateToProps = (state) => {
-  return { users: state.users }
+  return { users: state.users, userCount: state.userCount }
 }
 
 
 export default connect(mapStateToProps)(Users);
 
-
+//  return Object.assign({}, state, { items: state.items.concat(state.items.length + 1) })
 //export default connect()(Users);
--- src/reducers/manageUsers.js
+++ src/reducers/manageUsers.js
@@ -1,12 +1,13 @@
 export default function manageUsers(state = {
-  users: [],
+  users: [], userCount: 0
 }, action){
   switch (action.type) {
     case 'ADD_USER':
+      //debugger;
       console.log('adding ', action.user);
       return {
         ...state,
-        users: [...state.users, action.user]
+        users: [...state.users, action.user], userCount: state.userCount + 1
       }
 
     default:
